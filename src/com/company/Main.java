5fa6e8e38c81a39de5fcc819a3f545c101375325
macbookpro <adiletismailov045@gmail.com>
2022-11-30 16:39:39 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int counter = 0;
+        String[] alphabets = {"A a", "B b", "C c", "D d", "E e", "F f", "G g", "H h", "I i", "J j", "K k", "L l", "M m",
+                "N n", "O o ", "P p", "Q q ", "R r", "S s", "T t ", "U u", "V v", "W w", "X x", "Y y", "Z z"};
+
+        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        try (FileWriter writer = new FileWriter("alphabets")){
+            for (String alphabet : alphabets) {
+                writer.write(alphabet + "\n");
+            }
+            for (int number : numbers) {
+                writer.write(number + "\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try (FileReader fileReader = new FileReader("alphabets")){
+            Scanner scanner = new Scanner(fileReader);
+            while (scanner.hasNextLine()) {
+                counter++;
+                System.out.println(counter + ": " + scanner.nextLine());
+            }
+        } catch (InputMismatchException e) {
+            e.printStackTrace();
+        } catch (Exception e){
+            System.out.println("Oshibka");
+        }
+    }
+}
